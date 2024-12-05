5f99f04d1713bad1fe4bbf287f4b7bada4f691ad
Suhas822 <jadhavsuhas822@gmail.com>
2024-12-05 20:15:32 +0530

    finding the ip and country is integrated

--- blog/utils.py
+++ blog/utils.py
@@ -1,6 +1,12 @@
+import requests
+
 def get_client_ip(request):
-    # Extract the client's IP from the request
-    ip = None
+    """Get the IP address of the client from the request."""
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
     return ip
 
 def get_public_ip():
@@ -8,12 +14,13 @@ def get_public_ip():
     public_ip = None
     return public_ip
 
-def get_user_country(request):
-    # Check if the request is coming from localhost or public domain
-    ip = get_client_ip(request)
-    # If the IP is from localhost, use the public IP service
 
-    # Get country information based on the detected IP
-    country = None
-
-    return country
+def get_user_country(ip_address):
+    try:
+        response = requests.get(f"http://ip-api.com/json/{ip_address}")
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('country', 'Unknown')
+    except Exception as e:
+        print(f"Error fetching country: {e}")
+    return 'Unknown'
--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,14 @@ from django.shortcuts import render
 from .models import BlogPost
 
 from .utils import get_user_country
+from .utils import get_client_ip,get_user_country
+
 
 def post_list(request):
-    country = get_user_country(request=request)
-
+  
+    ip_address = get_client_ip(request)
+    country = get_user_country(ip_address)
+    
     posts = BlogPost.objects.all()
     filtered_posts = posts.filter(country=country)
-
-    return render(request, 'blog/post_list.html', {
-        'posts': posts,
-        'filtered_posts': filtered_posts,
-        'country': country,
-    })
-
+    return render(request, 'blog/post_list.html', {'posts': filtered_posts,'user_country': country})
